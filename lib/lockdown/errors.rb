0b9411dfc63445dc30953de90a1588706947291b
Andrew Stone <andy@stonean.com>
2009-11-27 19:31:01 -0500

    add GroupUndefinedError

--- lib/lockdown/errors.rb
+++ lib/lockdown/errors.rb
@@ -6,4 +6,6 @@ module Lockdown
   class PermissionScopeCollision < StandardError; end
 
   class InvalidPermissionAssignment < StandardError; end
+
+  class GroupUndefinedError < StandardError; end
 end
