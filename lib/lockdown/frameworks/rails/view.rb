444da61e9c92972621ce974a177fb8f0240166e5
Andrew Zielinski <andrew@iplayup.com>
2009-12-18 16:12:41 +1100

    Added link_to_remote and button_to_remote permissions checks

--- lib/lockdown/frameworks/rails/view.rb
+++ lib/lockdown/frameworks/rails/view.rb
@@ -7,36 +7,35 @@ module Lockdown
             alias_method :link_to_open, :link_to
             alias_method :link_to, :link_to_secured
 
+            alias_method :link_to_remote_open, :link_to_remote
+            alias_method :link_to_remote, :link_to_remote_secured
+
             alias_method :button_to_open, :button_to
             alias_method :button_to, :button_to_secured
+
+            alias_method :button_to_remote_open, :button_to_remote
+            alias_method :button_to_remote, :button_to_secured
+
           end
         end
-    
+
         def link_to_secured(name, options = {}, html_options = nil)
-          url = url_for(options)
-
-          method = html_options ? html_options[:method] : :get
-
-         url_to_authorize = remove_subdirectory(url)
-
-         if authorized?(url_to_authorize, method)
-            return link_to_open(name, url, html_options)
-          end
-          return ""
+          secured_link_for(:link_to_open, name, options, html_options)
         end
+
+        def link_to_remote_secured(name, options = {}, html_options = nil)
+          secured_link_for(:link_to_remote_open, name, options, html_options)
+        end
+
 
         def button_to_secured(name, options = {}, html_options = nil)
-          url = url_for(options)
-
-          method = html_options ? html_options[:method] : :get
-
-          url_to_authorize = remove_subdirectory(url)
-
-          if authorized?(url_to_authorize, method)
-            return button_to_open(name, url, html_options)
-          end
-          return ""
+          secured_link_for(:button_to_open, name, options, html_options)
         end
+
+        def button_to_remote_secured(name, options = {}, html_options = nil)
+          secured_link_for(:button_to_remote_open, name, options, html_options)
+        end
+
 
         def link_to_or_show(name, options = {}, html_options = nil)
           lnk = link_to(name, options, html_options)
@@ -49,11 +48,28 @@ module Lockdown
           rvalue.join( Lockdown::System.fetch(:link_separator) )
         end
 
-        
+        def secured_link_for(link_method_name, name, options, html_options)
+          url = url_from(options)
+          method = html_options ? html_options[:method] : :get
+          url_to_authorize = remove_subdirectory(url)
+          if authorized?(url_to_authorize, method)
+            return send(link_method_name, name, options, html_options)
+          end
+          return ""
+        end
+
+        private
+
         def remove_subdirectory(url)
           subdir = Lockdown::System.fetch(:subdirectory)
           subdir ? url.gsub(/^\/?#{subdir}/,'') : url
         end
+
+        def url_from(options)
+          url = options.is_a?(Hash) ? options[:url] || options : options
+          url_for(url)
+        end
+
 
       end # View
     end # Rails
--- spec/lockdown/frameworks/rails/view_spec.rb
+++ spec/lockdown/frameworks/rails/view_spec.rb
@@ -9,6 +9,14 @@ class TestAView
     "button_to"
   end
 
+  def link_to_remote
+    "link_to_remote"
+  end
+
+  def button_to_remote
+    "button_to_remote"
+  end
+
   include Lockdown::Frameworks::Rails::View
 end
 
@@ -43,7 +51,26 @@ describe Lockdown::Frameworks::Rails::Controller do
 
   end
 
+  describe "#link_to_remote_secured" do
+    it "should return the link if authorized" do
+      link = "<a href='http://a.com'>my_link</a>"
+      @view.stub!(:authorized?).and_return(true)
+      @view.stub!(:link_to_remote_open).and_return(link)
+      @view.link_to_remote_secured("my link", @options).should == link
+    end
 
+    it "should return an empty string if authorized" do
+      @view.stub!(:authorized?).and_return(false)
+      @view.link_to_remote_secured("my link", @options).should == ""
+    end
+
+    it "should attempt to remove a subdirectory if it exists" do
+      @view.should_receive(:remove_subdirectory).once
+      @view.stub!(:authorized?).and_return(false)
+      @view.link_to_remote_secured("my link", @options).should == ""
+    end
+
+  end
 
 
   describe "#button_to_secured" do
@@ -69,6 +96,28 @@ describe Lockdown::Frameworks::Rails::Controller do
 
 
   end
+
+  describe "#button_to_remote_secured" do
+    it "should return the link if authorized" do
+      link = "<a href='http://a.com'>my_link</a>"
+      @view.stub!(:authorized?).and_return(true)
+      @view.stub!(:button_to_remote_open).and_return(link)
+      @view.button_to_remote_secured("my link", @options).should == link
+    end
+
+    it "should return an empty string if authorized" do
+      @view.stub!(:authorized?).and_return(false)
+      @view.button_to_remote_secured("my link", @options).should == ""
+    end
+
+    it "should attempt to remove a subdirectory if it exists" do
+      @view.should_receive(:remove_subdirectory).once
+      @view.stub!(:authorized?).and_return(false)
+      @view.button_to_remote_secured("my link", @options).should == ""
+    end
+
+  end
+
 
   describe "#link_to_or_show" do
     it "should return the name if link_to returned an empty string" do
@@ -108,18 +157,38 @@ describe Lockdown::Frameworks::Rails::Controller do
     end
 
     it "should remove subdirectory /test" do
-      @view.remove_subdirectory('/test/posts/new').should == '/posts/new'
+      @view.send(:remove_subdirectory,'/test/posts/new').should == '/posts/new'
     end
 
     it "should remove subdirectory 'test' without a leading /" do
-      @view.remove_subdirectory('test/posts/new').should == '/posts/new'
+      @view.send(:remove_subdirectory,'test/posts/new').should == '/posts/new'
     end
 
     it "should leave the url untouched" do
-      @view.remove_subdirectory('/posts/new').should == '/posts/new'
+      @view.send(:remove_subdirectory,'/posts/new').should == '/posts/new'
     end
 
 
   end
+
+  describe "#url_from" do
+
+    it "should derive the path from the :url if given with options" do
+      options = { :url => 'test/test' }
+      @view.should_receive(:url_for).with(options[:url])
+      @view.should_not_receive(:url_for).with(options)
+      @view.send(:url_from, options)
+    end
+
+    it "should derive the path from the options hash if no :url is given" do
+      options = { :controller => 'test', :action => 'index' }
+      @view.should_receive(:url_for).with(options)
+      @view.send(:url_from, options)
+    end
+
+
+  end
+
+
 
 end
